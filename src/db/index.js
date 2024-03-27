bcf59bfa67d7bca3681b735d0e548a76b363db9c
divykaila <kailadivy098@gmail.com>
2024-03-27 18:55:23 +0530

    Add CORS support , cookie-parser and other required middleware

--- src/db/index.js
+++ src/db/index.js
@@ -6,14 +6,11 @@ const connectDB = async () => {
     try {
 
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-        // console.log('connectionInstance :-', connectionInstance);
-        // console.log('connectionInstance.connections :- ', connectionInstance.connection);
         console.log('MongoDB connected:', connectionInstance.connection.host);
 
     } catch (error) {
         console.log("mongoDB connection Error", error);
         process.exit(1);
-        // throw error;
     }
     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
-// require("dotenv").config({ path: "./.env" });
-// import dotenv from "dotenv/config";
+import { app } from "./app.js";
+
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import connectDB from "./db/index.js";
 
-connectDB();
-
-/*
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
-const app = express();
-
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI} /${DB_NAME}`);
+connectDB()
+    .then(() => {
         app.on("error", (error) => {
             console.log("Error", error);
             throw error;
-        })
+        });
 
         app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.log("Error", error);
-    }
-})();
-*/
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("mongoDb connection failed!", error);
+    });
