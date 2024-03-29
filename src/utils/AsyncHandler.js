33bac693a36b127cf349beccd2098bf577bb5d4f
divykaila <kailadivy098@gmail.com>
2024-03-29 17:33:52 +0530

    setup routes , controllers  and fix issue in asyncHandler file

--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,5 +1,5 @@
 const AsyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => {
       res.status(err.status || 500).json({
         success: false,
