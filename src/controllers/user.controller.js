f8609bfe95986ee5d4db1ec9a1f195dfe0b902a0
divykaila <kailadivy098@gmail.com>
2024-03-29 18:49:20 +0530

    add register controller and fix some other issues

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,78 @@
 import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
+// import { validationResult } from "express-validator";
 
 const registerUser = AsyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  //   const errors = validationResult(req);
+  //   if (!errors.isEmpty()) {
+  //     return res.status(400).json({ errors: errors.array() });
+  //   }
+
+  // get user details from front-end
+  const { fullName, email, username, password } = req.body;
+  console.log(email);
+
+  // validation - not empty
+  if (
+    [fullName, email, username, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // check if user is alredy exists: username, email
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "user with this email or username alredy exists");
+  }
+  // if(await User.exists({ email })) {
+  //     throw new ApiError(400, "User already exists");
+  // }
+
+  //  check for avatar and coverImage
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+
+  // upload them to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  //   create user object - create entry in db
+  const user = await User.create({
+    fullName,
+    email,
+    username: username.toLowerCase(),
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  //   remove password and refreshToken from field
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // check for user creation
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering user");
+  }
+  // return res
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "user created successfully"));
 });
 
 export { registerUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,27 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+// import { body } from "express-validator";
 
 const router = Router();
-router.route("/register").post(registerUser);
+// router
+//   .route("/register")
+//   .post(
+//     [body("email").notEmpty(), body("password","password lenght must be between 5 to 10").isLength({ min: 5, max: 10 })],
+//     registerUser
+//   );
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
