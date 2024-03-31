2c23264d0f07c329e924413aab105d81fb224196
divykaila <kailadivy098@gmail.com>
2024-03-31 20:30:05 +0530

    Refactor database connection and server initialization not and issue or bug

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
 
 connectDB()
-    .then(() => {
-        app.on("error", (error) => {
-            console.log("Error", error);
-            throw error;
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log("mongoDb connection failed!", error);
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
     });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDb connection failed!", error);
+  });
